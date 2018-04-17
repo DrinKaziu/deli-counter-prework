d3c8590bee4b7dc69a8d611539269ba59fa3af6e
Drin K <drinkaziu@gmail.com>
2018-04-17 20:07:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  while katz_deli.size < 1 
+  unless katz_deli.size > 0  
     puts "There is nobody waiting to be served!"
   end
   puts "Currently serving #{katz_deli.shift}."
