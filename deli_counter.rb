3106ee12319c33d65139591e1e1bf231f5cb5ed8
Drin K <drinkaziu@gmail.com>
2018-04-17 19:58:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-# def now_serving(katz_deli)
-#   "Currently serving #{katz_deli.shift}."
-# end
+def now_serving(katz_deli)
+  "Currently serving #{katz_deli.shift}."
+end
 
