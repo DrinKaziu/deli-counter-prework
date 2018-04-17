eaa8362be3677876924d5f6b7e0f7287973f116a
Drin K <drinkaziu@gmail.com>
2018-04-17 19:58:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.last} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 # def now_serving(katz_deli)
